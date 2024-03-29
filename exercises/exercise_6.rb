b8f0bc7cb9f464d76f9668055d70f8643dab804b
Alysha Huot <alyshajhuot@gmail.com>
2024-03-29 11:06:07 -0600

    complete exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,22 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: 'Khurram', last_name: 'Virani', hourly_rate: 20)
+@store1.employees.create(first_name: 'Jeremy', last_name: 'Huot', hourly_rate: 90)
+@store1.employees.create(first_name: 'Jenica', last_name: 'OToole', hourly_rate: 30)
+@store1.employees.create(first_name: 'Praneet', last_name: 'Akilla', hourly_rate: 30)
+@store1.employees.create(first_name: 'Ivy', last_name: 'Holliday', hourly_rate: 25)
+@store1.employees.create(first_name: 'Riva', last_name: 'Anwari', hourly_rate: 40)
+@store1.employees.create(first_name: 'Noah', last_name: 'Maddock', hourly_rate: 30)
+
+@store2.employees.create(first_name: 'Kassia', last_name: 'Selinger', hourly_rate: 45)
+@store2.employees.create(first_name: 'Kevin', last_name: 'Szydlowski', hourly_rate: 75)
+@store2.employees.create(first_name: 'Liam', last_name: 'Amyotte', hourly_rate: 45)
+@store2.employees.create(first_name: 'Michael', last_name: 'Roundtree', hourly_rate: 35)
+@store2.employees.create(first_name: 'Noel', last_name: 'Banda', hourly_rate: 45)
+@store2.employees.create(first_name: 'Miriam', last_name: 'Isaac', hourly_rate: 20)
+@store2.employees.create(first_name: 'Mackenzie', last_name: 'Johnson', hourly_rate: 50)
